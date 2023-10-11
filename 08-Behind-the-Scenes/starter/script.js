b2e71e042e6716602b708aeeb339d357cf363ac5
ArthurADDDDD <a535483659@gmail.com>
2023-10-11 17:55:43 +0800

    sync

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -17,17 +17,36 @@
 // const x = first();
 // console.log(x);
 
-function calcAge(birthYear) {
-    const age = 2023 - birthYear;
+// function calcAge(birthYear) {
+//     const age = 2023 - birthYear;
 
-    function printAge() {
-        const output = `You are ${age} years old, and you born in ${birthYear}!`
-        console.log(output)
-    }
-    printAge();
-    return age;
+//     function printAge() {
+//         const output = `You are ${age} years old, and you born in ${birthYear}!`
+//         console.log(output)
+//     }
+//     printAge();
+//     return age;
+// }
+
+// calcAge(1991);
+// const test = calcAge(1991);
+// console.log(calcAge(1991), test, typeof test)
+
+
+function test() {
+    console.log(this)
 }
 
-calcAge(1991);
-const test = calcAge(1991);
-console.log(calcAge(1991), test, typeof test)
+test();
+
+const testExr = function () {
+    console.log(this)
+}
+
+testExr();
+
+const testArrow = () => {
+    console.log(this)
+}
+
+testArrow();
